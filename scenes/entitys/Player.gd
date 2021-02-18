6cd7641228454eff57c3e2198c292cd3ccb6a189
Michael Schertenleib <michaelschertenleib@gmail.com>
2021-02-18 22:43:14 +0100

    Add level border

--- scenes/entitys/Player.gd
+++ scenes/entitys/Player.gd
@@ -34,13 +34,36 @@ func _physics_process(_delta: float) -> void:
 		
 	if left or right or up or down:
 		$AnimatedSprite.animation = "flying"
+		$AnimatedSpriteClone.animation = "flying"
 	else:
 		$AnimatedSprite.animation = "default"
+		$AnimatedSpriteClone.animation = "flying"
 	
 	velocity.x = clamp(velocity.x, -max_speed, max_speed)
 	velocity.y = clamp(velocity.y, -max_speed, max_speed)
 	
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
+	var viewport_size = get_viewport_rect().size \
+			/ get_canvas_transform().get_scale()
+	var half_viewport_size = viewport_size / 2
+	var half_height = $CollisionShape2D.shape.height / 2 \
+			+ $CollisionShape2D.shape.radius
+	
+	position.y = clamp(position.y, -half_viewport_size.y + half_height,
+			half_viewport_size.y - half_height)
+	
+	$AnimatedSpriteClone.global_position = position
+	
+	if position.x < -half_viewport_size.x:
+		position.x += viewport_size.x
+	elif position.x > half_viewport_size.x:
+		position.x -= viewport_size.x
+	
+	if position.x < 0:
+		$AnimatedSpriteClone.global_position.x += viewport_size.x
+	else:
+		$AnimatedSpriteClone.global_position.x -= viewport_size.x
 	
 	if is_shooting:
 		$StandardWeapon.shoot()
