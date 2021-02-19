d3a85b2b30ffd3467cc525d84cc229be21624861
timmmmmb <frey.tim@outlook.com>
2021-02-19 21:07:01 +0100

    added a boss and target searching rockets

---
+++ scenes/entities/Boss.gd
@@ -0,0 +1,9 @@
+extends Enemy
+
+var mg
+var rocket_launcher
+var shotgun
+var stage = 1
+
+enum BOSSSTATE {IDLE, MOVING, MG_ATTACKING, ROCKET_ATTACKING, SHOTGUN_ATTACKING}
+
---
+++ scenes/weapons/RocketWeapon.gd
@@ -0,0 +1,13 @@
+extends StandardWeapon
+
+func spawn_bullet() -> void:
+	$Sound.play(0)
+	var projectile = Rocket.instance()
+	if get_tree().get_nodes_in_group("player").size() >= 1:
+		projectile.target = get_tree().get_nodes_in_group("player")[0]
+
+	#projectile.position = self.position + get_parent().position
+	#projectile.rotation = self.rotation
+	projectile.global_position = global_position
+	projectile.rotation = global_rotation
+	get_tree().root.get_child(0).add_child(projectile)
